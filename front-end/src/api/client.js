d99fb196cfdde78807e81f4116a248749ec4c465
youssouf <y_bouderbala@estin.dz>
2026-05-04 21:43:20 +0100

    deployment

--- front-end/src/api/client.js
+++ front-end/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL+"/api" || "http://localhost:5003/api").replace(/\/+$/, "");
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:5003/api").replace(/\/+$/, "");
 
 const api = axios.create({
   baseURL: API_BASE_URL,
